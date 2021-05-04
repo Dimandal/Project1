9af2640adc0b5ae5746a034286292ad0dca46779
Dimandal <dimandal.92@gmail.com>
2021-05-04 17:43:44 +0530

    first commit

---
+++ grade.sh
@@ -0,0 +1,19 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+#echo "Enter the name of the student: "
+#echo "Enter the marks of the student: "
+
+read n  
+read m
+if [ $m -gt 100 ]
+then
+    echo "The grade of student named $n is A"
+elif [ $m -gt 80 ]
+then    
+    echo "The grade of student named $n is A"
+elif [ $m -gt 40 ]
+then    
+    echo "The grade of student named $n is C"
+else    
+    echo "The grade of student named $n is C"
+fi
+
